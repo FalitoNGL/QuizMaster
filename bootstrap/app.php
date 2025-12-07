7e9597faff1606f2232795be7400cafad0201ef2
Falito Eriano <falitoeriano17@gmail.com>
2025-12-07 13:56:13 +0700

    FalitoNGL

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\TrustProxies; // <--- Tambahkan baris ini di paling atas!
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Tambahkan kode ini:
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: ['*']);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
